ce05d40758e5d457b5ed2802d1873150e81a50a7
KimSangHyun <ksh9891@gmail.com>
2019-12-30 14:57:56 +0900

    project init

---
+++ triptogether-api/src/main/java/com/toyproject/api/triptogetherapi/TriptogetherApiApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.api.triptogetherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptogetherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriptogetherApiApplication.class, args);
+    }
+
+}
---
+++ triptogether-api/src/test/java/com/toyproject/api/triptogetherapi/TriptogetherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.api.triptogetherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptogetherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
